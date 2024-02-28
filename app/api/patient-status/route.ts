ea703f7ff5936a7c31db0d4be39089babc8ac5d5
armsves <armsves@gmail.com>
2024-02-28 13:19:38 +0100

    trasteando con el device status4

--- app/api/patient-status/route.ts
+++ app/api/patient-status/route.ts
@@ -5,15 +5,17 @@ export async function POST(request: Request) {
   const { searchParams } = new URL(request.url);
   console.log(searchParams)
 
-  const notification = searchParams.get('notification');
+  const notification = searchParams.get('eventType');
   const notification2 = searchParams.get('eventType');
   const notification3 = searchParams.get('deviceStatus');
-  
+  const notification4 = searchParams.get('');
+
   try {
     //if (!petName || !ownerName) throw new Error('Pet and owner names required');
     await sql`INSERT INTO history (id_patient, history_event) VALUES (1, ${notification});`;
     await sql`INSERT INTO history (id_patient, history_event) VALUES (2, ${notification2});`;
     await sql`INSERT INTO history (id_patient, history_event) VALUES (3, ${notification3});`;
+    await sql`INSERT INTO history (id_patient, history_event) VALUES (1, ${notification4});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
