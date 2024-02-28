4ae2b5dbf35e95027ecdd335fb4f9493666a2c94
armsves <armsves@gmail.com>
2024-02-28 11:56:11 +0100

    trasteando con el device status

--- app/api/patient-status/route.ts
+++ app/api/patient-status/route.ts
@@ -3,11 +3,12 @@ import { NextResponse } from 'next/server';
  
 export async function POST(request: Request) {
   const { searchParams } = new URL(request.url);
-  const notification = searchParams.get('notification');
+  const notification = searchParams.get('Notification');
+  
 
   try {
     //if (!petName || !ownerName) throw new Error('Pet and owner names required');
-    await sql`INSERT INTO history (id_patient, history_event) VALUES (2, ${notification});`;
+    await sql`INSERT INTO history (id_patient, history_event) VALUES (1, ${notification});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
