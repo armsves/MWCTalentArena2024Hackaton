340a64157c8f2e030436563d44078e52cb110eaf
armsves <armsves@gmail.com>
2024-02-28 10:05:39 +0100

    endpoint added y changed to post

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Suspense } from 'react'
-import Table from '@/components/table'
+import Table from '@/components/geofencing'
 import TablePlaceholder from '@/components/table-placeholder'
 import ExpandingArrow from '@/components/expanding-arrow'
 
@@ -15,12 +15,15 @@ export default function Home() {
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
         Worldwide Patient Records
       </h1>
+      <h2 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
+        Geofencing showcase
+      </h2>
       <Suspense fallback={<TablePlaceholder />}>
-        <Table/>
+        <Table />
       </Suspense>
       <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6">
 
-        
+
       </p>
 
       <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
