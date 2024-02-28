aea3fb927dd777c19cc2cb2b0b7a0a111924042e
armsves <armsves@gmail.com>
2024-02-28 14:03:46 +0100

    sigo con los webhooks

--- app/api/patient-status/route.ts
+++ app/api/patient-status/route.ts
@@ -3,19 +3,14 @@ import { NextResponse } from 'next/server';
  
 export async function POST(request: Request) {
   const { searchParams } = new URL(request.url);
-  console.log(searchParams)
-
-  const notification = searchParams.get('connectivity');
-  const notification2 = searchParams.get('notifications');
-  const notification3 = searchParams.get('notification');
-  const notification4 = searchParams.get('device-status');
+  const notification = searchParams.get('event.deviceStatus');
+  const notification2 = searchParams.get('event');
 
   try {
     //if (!petName || !ownerName) throw new Error('Pet and owner names required');
     await sql`INSERT INTO history (id_patient, history_event) VALUES (1, ${notification});`;
     await sql`INSERT INTO history (id_patient, history_event) VALUES (2, ${notification2});`;
-    await sql`INSERT INTO history (id_patient, history_event) VALUES (3, ${notification3});`;
-    await sql`INSERT INTO history (id_patient, history_event) VALUES (1, ${notification4});`;
+
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
