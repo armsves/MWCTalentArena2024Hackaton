3ee29e918ff07f96853bcbc4ee7367e45af29e6c
armsves <armsves@gmail.com>
2024-02-28 14:08:35 +0100

    sigo con los webhooks

--- app/api/patient-status/route.ts
+++ app/api/patient-status/route.ts
@@ -5,11 +5,14 @@ export async function POST(request: Request) {
   const { searchParams } = new URL(request.url);
   const notification = searchParams.get('event.deviceStatus');
   const notification2 = searchParams.get('event');
+  const notification3 = searchParams.get('deviceStatus');
+  
 
   try {
     //if (!petName || !ownerName) throw new Error('Pet and owner names required');
     await sql`INSERT INTO history (id_patient, history_event) VALUES (1, ${notification});`;
     await sql`INSERT INTO history (id_patient, history_event) VALUES (2, ${notification2});`;
+    await sql`INSERT INTO history (id_patient, history_event) VALUES (2, ${notification3});`;
 
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
